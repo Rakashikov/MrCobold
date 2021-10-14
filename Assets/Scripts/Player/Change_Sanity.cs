61c582339936c2c82bc96de6f88da98749c4c26e
Evgeniy Rakashikov <evgeniyhd@gmail.com>
2021-10-14 16:18:14 +0700

    fix change_sanity

--- Assets/Scripts/Player/Change_Sanity.cs
+++ Assets/Scripts/Player/Change_Sanity.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Rendering.PostProcessing;
@@ -13,39 +12,43 @@ public enum StateChangeTexture
 
 public class Change_Sanity : MonoBehaviour
 {
+	[Header("Assignables")]
 	[SerializeField] private Slider sanity;
+	[SerializeField] private MovementBuka Buka;
 	[SerializeField] private Transform orientation;
 	[SerializeField] private Light sun;
 	[SerializeField] private PostProcessVolume pp;
+
+	[Header("Variables")]
 	[SerializeField] private float forceChange = 0.01f;
 	[SerializeField] private float maxForce = 0.07f;
-	[SerializeField] private float timeChange = 0.1f;
-	[SerializeField] private MovementBuka Buka;
 	[SerializeField] private float valueToSpawnBuka = 0f;
-	[SerializeField] private float changeLightSpeed = 1f;
-	[SerializeField] private Transform PointBukaSpawn;
+	[SerializeField] private float changeLightSpeed = 5f;
 
 	public UnityAction<StateChangeTexture> OnChangeSanity;
 
 	private Player_Caress_Cat playerCC;
-	public float SanityValue { set; get; }
-	Vignette vignetteLayer = null;
+	private float SanityValue;
+	private Vignette vignetteLayer = null;
 
 	private bool _bukaSpawns;
 
 	private void Start() {
-		RenderSettings.ambientIntensity = 2f;
 		playerCC = GetComponent<Player_Caress_Cat>();
+
 		SanityValue = sanity.value;
 		playerCC.OnIncreaseSanity += IncreaseSanity;
 		pp.profile.TryGetSettings(out vignetteLayer);
+
 		StartCoroutine(nameof(DecreaseSanity));
 	}
 
-    private void Update()
+    private void FixedUpdate()
     {
+		//IncreaseDifficulty
 		forceChange = Mathf.Min(forceChange + Time.deltaTime / 1000f, maxForce);
     }
+
 
     private IEnumerator DecreaseSanity() {
 		while(true) {
@@ -53,13 +56,21 @@ public class Change_Sanity : MonoBehaviour
 			vignetteLayer.intensity.value = 0.5f - sanity.value / 2;
 			SanityValue = Mathf.Max(SanityValue - forceChange * Time.deltaTime, 0f);
 			sanity.value = SanityValue;
+
 			if(sanity.value <= valueToSpawnBuka && !_bukaSpawns) {
+				//ChangeTexture
 				OnChangeSanity?.Invoke(StateChangeTexture.finalTexture);
+				
+				//SpawnBuka
 				Buka.gameObject.SetActive(true);
 				Buka.transform.localPosition = transform.localPosition - orientation.transform.forward * 10;
 				_bukaSpawns = true;
-				StopCoroutine("ChangeLight");
-				StartCoroutine("ChangeLight", 0.1f);
+
+				//ChangeLight
+				StopCoroutine(nameof(ChangeLight));
+				StartCoroutine(nameof(ChangeLight), 0.1f);
+
+				//Audio
 				FindObjectOfType<AudioLevelManager>().Play("DarkAmbient");
 				FindObjectOfType<AudioLevelManager>().Play("DarkMusic");
 				FindObjectOfType<AudioLevelManager>().StopPlay("NormAmbient");
@@ -71,8 +82,8 @@ public class Change_Sanity : MonoBehaviour
     {
 		while (RenderSettings.ambientIntensity != value)
         {
-			RenderSettings.ambientIntensity += (value - RenderSettings.ambientIntensity) / 100f * Time.deltaTime * changeLightSpeed;
-			sun.intensity += (value/2 - sun.intensity) / 100f * Time.deltaTime * changeLightSpeed;
+			RenderSettings.ambientIntensity = Mathf.Lerp(RenderSettings.ambientIntensity, value, Time.deltaTime * changeLightSpeed);
+			sun.intensity = Mathf.Lerp(sun.intensity, value, Time.deltaTime * changeLightSpeed);
 			yield return null;
         }
     }
@@ -80,12 +91,20 @@ public class Change_Sanity : MonoBehaviour
 	private void IncreaseSanity(float valueChange) {
 		SanityValue = Mathf.Min(SanityValue + valueChange,1);
 		sanity.value = SanityValue;
+
 		if(sanity.value > valueToSpawnBuka && _bukaSpawns) {
+			//ChangeTexture
 			OnChangeSanity?.Invoke(StateChangeTexture.startTexture);
+
+			//DespawnBuka
 			Buka.gameObject.SetActive(false);
 			_bukaSpawns = false;
-			StopCoroutine("ChangeLight");
-			StartCoroutine("ChangeLight", 2f);
+
+			//ChangeLight
+			StopCoroutine(nameof(ChangeLight));
+			StartCoroutine(nameof(ChangeLight), 1.5f);
+
+			//Audio
 			FindObjectOfType<AudioLevelManager>().StopPlay("DarkAmbient");
 			FindObjectOfType<AudioLevelManager>().StopPlay("DarkMusic");
 			FindObjectOfType<AudioLevelManager>().Play("NormAmbient");
